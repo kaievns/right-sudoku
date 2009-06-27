f14c5764e860b7397e367efa52a477b6ba6550dc
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-06-27 14:42:07 +0400

    initial commit

---
+++ src/init.js
@@ -0,0 +1,5 @@
+[
+  'sudoku'
+].each(function(name) {
+  document.writeln('<scr'+'ipt type="text/javascript" src="src/'+name+'.js"></scr'+'ipt>');
+});
---
+++ src/sudoku.js
@@ -0,0 +1,10 @@
+/**
+ * RightJS based Sudoku game
+ *
+ * @copyright Nikolay V. Nemshilov aka St. <nemshilov%gmail#com>
+ */
+var Sudoku = new Class({
+  initialize: function(element) {
+    this.container = $(element);
+  }
+})
